9648766a3dced70a6e49807a2faa3fad76b4586c
Gijs101 <86304343+Gijs101@users.noreply.github.com>
2021-11-08 11:29:52 +0100

    Update Dialog.vue
    Changed header title h5 to h4

--- src/components/Dialog/Dialog.vue
+++ src/components/Dialog/Dialog.vue
@@ -3,7 +3,7 @@
         <div class="modal-dialog " :class="centered ? 'modal-dialog-centered' : ''">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">{{ title }}</h5>
+                    <h2 class="modal-title">{{ title }}</h2>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" v-html="message"></div>
